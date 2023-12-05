85f78112f3c01135de8754358f72c8a2a6dacc49
PhamThi1710 <lsjenny1710@gmail.com>
2023-12-05 14:20:15 +0700

    activity log

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.set("view engine", "ejs");
 
 // usage
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use("/img", express.static(path.join(__dirname, "img")));
+app.use("/themify-icons",express.static(__dirname + '/themify-icons'));
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 
 // route
-app.use('/', require('./routes/newReq'));
+app.use('/', require('./routes/ActivityLog'));
 
 
 // start server
