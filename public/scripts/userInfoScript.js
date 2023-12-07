57ea50e062cb3ac3f8d0ee31cd81dc020309d054
TruogAnh-28 <106266235+TruogAnh-28@users.noreply.github.com>
2023-12-07 14:57:18 +0700

    Update userInfoScript.js

--- public/scripts/userInfoScript.js
+++ public/scripts/userInfoScript.js
@@ -95,42 +95,86 @@ async function getUserpages() {
     });
 };
 getUserpages();
+async function getUserPhone() {
+    var user_inf = document.getElementById('student-inf.phone');
+    if (user_inf.innerHTML.length > 0) return;
+    await fetch("http://127.0.0.1:3000/users/profile", {
+        method: "GET",
+        headers: {
+            "Authorization": "655487f17a305c1ebe99a11f eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5lc3RreW84MkBnbWFpbC5jb20iLCJfaWQiOiI2NTU0ODdmMTdhMzA1YzFlYmU5OWExMWYiLCJzdHVkZW50aWQiOiIxMjM0NTYiLCJpYXQiOjE3MDE3NDA1MDUsImV4cCI6MTcwMjM0NTMwNX0.ygTuhyUflp3EQYoExGtVY5U8f3qmAx9_X-ycwobTPM4"
+        }
+    }).then(async (res) => {
+        user = await res.json();
+        user_inf.innerHTML = user.Phone;
+
+        // var pageleft = user.pages;
+        // getTotalPage(pageleft);
+    }).catch((err) => {
+        console.log(err);
+    });
+};
+getUserPhone();
+async function getUserDate() {
+    var user_inf = document.getElementById('student-inf.birthday');
+    if (user_inf.innerHTML.length > 0) return;
+    await fetch("http://127.0.0.1:3000/users/profile", {
+        method: "GET",
+        headers: {
+            "Authorization": "655487f17a305c1ebe99a11f eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5lc3RreW84MkBnbWFpbC5jb20iLCJfaWQiOiI2NTU0ODdmMTdhMzA1YzFlYmU5OWExMWYiLCJzdHVkZW50aWQiOiIxMjM0NTYiLCJpYXQiOjE3MDE3NDA1MDUsImV4cCI6MTcwMjM0NTMwNX0.ygTuhyUflp3EQYoExGtVY5U8f3qmAx9_X-ycwobTPM4"
+        }
+    }).then(async (res) => {
+        user = await res.json();
+
+        // Parse the date and format it (DD-MM-YYYY)
+        var birthday = new Date(user.Birthday);
+        var formattedDate = birthday.toLocaleDateString('en-GB', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric'
+        });
+
+        user_inf.innerHTML = formattedDate;
+    }).catch((err) => {
+        console.log(err);
+    });
+};
+getUserDate();
 // Mở form feedback khi click vào nút "Send Feedback"
-function openFeedbackForm() {
-    var feedbackForm = document.getElementById('feedback-form');
-    feedbackForm.style.display = 'block';
+// function openFeedbackForm() {
+//     var feedbackForm = document.getElementById('feedback-form');
+//     feedbackForm.style.display = 'block';
 
-    // Đồng thời ẩn nút "Send Feedback" để tránh click nhiều lần
-    var sendFeedbackBtn = document.getElementById('send-feedback-btn');
-    sendFeedbackBtn.style.display = 'none';
-}
+//     // Đồng thời ẩn nút "Send Feedback" để tránh click nhiều lần
+//     var sendFeedbackBtn = document.getElementById('send-feedback-btn');
+//     sendFeedbackBtn.style.display = 'none';
+// }
 
-// Đóng form feedback khi click vào nút "Close"
-function closeFeedbackForm() {
-    var feedbackForm = document.getElementById('feedback-form');
-    feedbackForm.style.display = 'none';
+// // Đóng form feedback khi click vào nút "Close"
+// function closeFeedbackForm() {
+//     var feedbackForm = document.getElementById('feedback-form');
+//     feedbackForm.style.display = 'none';
 
-    // Hiển thị lại nút "Send Feedback"
-    var sendFeedbackBtn = document.getElementById('send-feedback-btn');
-    sendFeedbackBtn.style.display = 'block';
-}
+//     // Hiển thị lại nút "Send Feedback"
+//     var sendFeedbackBtn = document.getElementById('send-feedback-btn');
+//     sendFeedbackBtn.style.display = 'block';
+// }
 
-// Gọi hàm closeFeedbackForm khi click vào overlay
-document.getElementById('feedback-overlay').addEventListener('click', function (event) {
-    if (event.target === this) {
-        closeFeedbackForm();
-    }
-});
+// // Gọi hàm closeFeedbackForm khi click vào overlay
+// document.getElementById('feedback-overlay').addEventListener('click', function (event) {
+//     if (event.target === this) {
+//         closeFeedbackForm();
+//     }
+// });
 
-// Gọi hàm closeFeedbackForm khi click vào nút "Close"
-document.getElementById('close-feedback-btn').addEventListener('click', function () {
-    closeFeedbackForm();
-});
+// // Gọi hàm closeFeedbackForm khi click vào nút "Close"
+// document.getElementById('close-feedback-btn').addEventListener('click', function () {
+//     closeFeedbackForm();
+// });
 
-// Gửi feedback (cần cập nhật hàm này tùy thuộc vào yêu cầu của bạn)
-function submitFeedback() {
-    // Thực hiện xử lý gửi feedback
-    // ...
-    // Sau khi xử lý xong, có thể đóng form feedback nếu cần
-    closeFeedbackForm();
-}
+// // Gửi feedback (cần cập nhật hàm này tùy thuộc vào yêu cầu của bạn)
+// function submitFeedback() {
+//     // Thực hiện xử lý gửi feedback
+//     // ...
+//     // Sau khi xử lý xong, có thể đóng form feedback nếu cần
+//     closeFeedbackForm();
+// }
